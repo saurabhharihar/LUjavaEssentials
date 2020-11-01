c5d56a56f4dd889467add9b4ea48c55d5b0f0152
saurabhharihar <61333233+saurabhharihar@users.noreply.github.com>
2020-11-01 11:00:21 +0530

    Add files via upload

---
+++ DAY 2/Employee.java
@@ -0,0 +1,11 @@
+package com.p1;
+public class Employee {
+	short age ;
+	String name;
+	String city;
+	public void display() {
+		System.out.println(" Name of employee : "+this.name);
+		System.out.println(" Age of employee : "+this.age);
+		System.out.println(" City of employee : "+this.city);
+	}
+}
---
+++ DAY 2/Main.java
@@ -0,0 +1,20 @@
+package com.p1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee e1=new Employee();
+		Employee e2=new Employee();
+		e1.name="Sangram";
+		e2.name="Rushi";
+		e1.age=20;
+		e2.age=18;
+		e1.city="Nalasopara";
+		e2.city="Atpadi";
+		
+		e1.display();
+		e2.display();
+	}
+
+}
